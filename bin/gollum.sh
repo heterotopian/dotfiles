8bd731133115a31bdd348247c6701fccc777067c
CS <cs@wahdis.com>
2013-08-22 12:31:28 -0400

    Fix incorrect script location

--- bin/gollum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-directory=$(dirname "${BASH_SOURCE[0]}")
-cd "${directory}"
-directory=$(readlink -f ..)
-page_directory="${directory}/gollum.wiki"
-echo cd "${page_directory}"
-cd "${page_directory}"
-nohup gollum --mathjax --port 4567 &> /dev/null &
-
