44494431a598cc150eeb71968176618f587c8e55
CS <cs@wahdis.com>
2013-08-22 12:45:50 -0400

    Change path to wiki directory

--- home/bin/gollum.sh
+++ home/bin/gollum.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-directory=$(dirname "${BASH_SOURCE[0]}")
-cd "${directory}"
-directory=$(readlink -f ..)
-page_directory="${directory}/gollum.wiki"
+page_directory=~/gollum.wiki
 echo cd "${page_directory}"
 cd "${page_directory}"
 nohup gollum --mathjax --port 4567 &> /dev/null &
